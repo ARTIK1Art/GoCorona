8d68b94ac451bcaac7c7f63720ad49cfb797b4d3
ARTIK1Art <98806951+ARTIK1Art@users.noreply.github.com>
2023-01-31 19:22:58 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.querySelector('.icon-menu').addEventListener('click', () => {
+	document.querySelector('.icon-menu').classList.toggle('_active');
+});
+document.querySelector('.icon-menu').addEventListener('click', () => {
+	document.querySelector('.menu__body').classList.toggle('top');
+});
+document.querySelector('.icon-menu').addEventListener('click', () => {
+	document.querySelector(".body").classList.toggle('lock');
+});
+document.querySelector('.icon-menu').addEventListener('click', () => {
+	document.querySelector(".get-started__image").classList.toggle('index');
+});
+document.querySelector('.icon-menu').addEventListener('click', () => {
+	document.querySelector(".get-started__container").classList.toggle('none');
+});
